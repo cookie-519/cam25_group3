589987a929195ac7b56151b363e8d83b67ab833e
cookie-519 <1472972590@qq.com>
2025-07-29 15:55:17 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,11 +3,23 @@ from torchvision import transforms
 from PIL import Image
 from model_def import Generator  # 替换为实际的Generator定义路径
 
-def load_model(model_path):
-    model = Generator()
-    model.load_state_dict(torch.load(model_path, map_location="cpu"))
-    model.eval()
-    return model
+def load_style_images(style_dir="dataset"):
+    from torchvision import transforms
+    images = []
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
+    for fname in os.listdir(style_dir):
+        if fname.endswith(('.jpg', '.png', '.jpeg')):
+            img = Image.open(os.path.join(style_dir, fname)).convert("RGB")
+            images.append(transform(img))
+    if images:
+        return torch.stack(images)
+    else:
+        return None
+
 
 def cartoonize(model, image):
     transform = transforms.Compose([
